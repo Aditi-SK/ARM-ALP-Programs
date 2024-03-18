2331036a94aa48f143f1a8a827111cb275e8d700
Aditi-SK <162660133+Aditi-SK@users.noreply.github.com>
2024-03-18 16:35:52 +0530

    Add files via upload

---
+++ strlen.asm
@@ -0,0 +1,11 @@
+				AREA STRLEN, CODE, READONLY
+				ENTRY
+				LDR R0,=MYSTRING				; load the address of the memory location of MYSTRING
+				MOV R1,#-1
+HERE		LDRB R2,[R0],#1					;load the byte data held by the memory location in R0 into the regiater R2
+				ADD R1,R1,#1						;increment the pointer R1
+				CMP R2,#0							;check if R2 ==0
+				BNE HERE							;branch to the label HERE if R2==0 
+	
+MYSTRING DCB "HELLO"
+				END
